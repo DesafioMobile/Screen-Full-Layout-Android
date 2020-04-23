98378d31e6a38accd421de81f1d97d84bdba000a
gilberto-code <gilberto.ribeiro@ufv.br>
2020-04-22 22:58:58 -0300

    Codigo pra printar tela e mostrar em ImageView

--- app/src/main/java/com/example/screenshotfulllayout/MainActivity.java
+++ app/src/main/java/com/example/screenshotfulllayout/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.screenshotfulllayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.widget.NestedScrollView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.pdf.PdfDocument;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -18,13 +26,55 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int PERMISSION_REQUEST_STORAGE = 1000;
+    private static final int READ_REQUEST_CODE = 42;
+    Button press;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == PERMISSION_REQUEST_STORAGE){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Permissão concedida!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permissão negada!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE);
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_STORAGE);
+        }
+
+        press =  findViewById(R.id.btn_add_sign);
+        press.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takeScreenShot();
+            }
+        });
+
     }
-    private String path;
+
+    public String path;
+    public String signature_pdf_ = "meupdf";
+    public String signature_img_ = "minhaimagem";
+    public int totalHeight;
+    public int totalWidth;
+    public File myPath;
+    public String imagesUri;
+    public Bitmap b;
+
     private void takeScreenShot() {
 
         File folder = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Signature/");
@@ -36,10 +86,10 @@ public class MainActivity extends AppCompatActivity {
         path = folder.getAbsolutePath();
         path = path + "/" + signature_pdf_ + System.currentTimeMillis() + ".pdf";// path where pdf will be stored
 
-        View u = findViewById(R.id.scroll);
-        NestedScrollView z = (NestedScrollView) findViewById(R.id.scroll); // parent view
-        totalHeight = z.getChildAt(0).getHeight();// parent view height
-        totalWidth = z.getChildAt(0).getWidth();// parent view width
+        //View u = findViewById(R.id.scroll);
+        View z = findViewById(R.id.scroll); // parent view
+        //totalHeight = z.getChildAt(0).getHeight();// parent view height
+        //totalWidth = z.getChildAt(0).getWidth();// parent view width
 
         //Save bitmap to  below path
         String extr = Environment.getExternalStorageDirectory() + "/Signature/";
@@ -50,7 +100,13 @@ public class MainActivity extends AppCompatActivity {
         myPath = new File(extr, fileName);
         imagesUri = myPath.getPath();
         FileOutputStream fos = null;
-        b = getBitmapFromView(u, totalHeight, totalWidth);
+
+
+
+
+        b = getBitmapFromView(z);
+
+//
 
         try {
             fos = new FileOutputStream(myPath);
@@ -58,11 +114,16 @@ public class MainActivity extends AppCompatActivity {
             fos.flush();
             fos.close();
 
+            ImageView tv1;
+            tv1= (ImageView) findViewById(R.id.imageView);
+            tv1.setImageBitmap(b);
+
         } catch (Exception e) {
             // TODO Auto-generated catch block
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             e.printStackTrace();
         }
-        createPdf();// create pdf after creating bitmap and saving
+        //createPdf();// create pdf after creating bitmap and saving
 
     }
     private void createPdf() {
@@ -96,5 +157,9 @@ public class MainActivity extends AppCompatActivity {
         document.close();
 
         //openPdf(path);// You can open pdf after complete
+    }
+    public Bitmap getBitmapFromView(View view){
+        view.setDrawingCacheEnabled(true);
+        return view.getDrawingCache();
     }
 }
